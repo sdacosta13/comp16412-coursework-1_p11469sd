2ffe1a1083b5147257434e7e1ce431af492d17fb
Sam Da Costa <samuel.dacosta-2@student.manchester.ac.uk>
2020-02-27 17:17:46 +0000

    on lab pc

--- run-tests.sh
+++ run-tests.sh
@@ -1,2 +1,3 @@
-javac -d ./chess/bin -cp .:junit-platform-console-standalone.jar ./chess/tests/*.java
-java -jar junit-platform-console-standalone.jar --class-path ./chess/bin --scan-class-path --fail-if-no-tests
+/usr/lib/jvm/java-11-openjdk/bin/javac -d ./chess/bin -cp .:junit-platform-console-standalone.jar ./chess/tests/*.java
+/usr/lib/jvm/java-11-openjdk/bin/java -jar junit-platform-console-standalone.jar --class-path ./chess/bin --scan-class-path --fail-if-no-tests
+
